b00a2323f2c14378235b0d29027933d43a22da6f
jackymeslin <jackymeslin@hotmail.com>
2020-04-10 14:41:52 +0200

    Ajax Form Validates Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,9 @@
 class Pet < ApplicationRecord
+  validates :photo , presence: true
+  validates :name , presence: true
+  validates :price_per_day , presence: true
+
+
   belongs_to :user
   has_one_attached :photo
   has_many :reservations, dependent: :destroy
